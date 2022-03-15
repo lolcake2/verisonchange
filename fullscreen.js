7ccdd25825876550dfee9d832d2db34cc8d86860
lolcake2 <95769152+lolcake2@users.noreply.github.com>
2022-03-15 11:29:17 -0700

    Create fullscreen.js

---
+++ fullscreen.js
@@ -0,0 +1,13 @@
+
+<script>
+var elem = document.getElementById("video.js");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+</script>
